eeb93ef2345622ba8e04aa36e4d9f3197fa4adc7
simharuk2021 <83026016+simharuk2021@users.noreply.github.com>
2021-10-16 10:33:30 +0100

    MODELS UPDATED
    Array brackets removed from director and genre models

--- models.js
+++ models.js
@@ -4,8 +4,8 @@ const bcrypt = require('bcrypt');
 let movieSchema = mongoose.Schema({
 Title: {type: String, required: true},
 Description: {type: String, required: true},
-Genre: [{type: mongoose.Schema.Types.ObjectId, ref: 'Genre'}],
-Director: [{type: mongoose.Schema.Types.ObjectId, ref: 'Director' }],
+Genre: {type: mongoose.Schema.Types.ObjectId, ref: 'Genre'},
+Director: {type: mongoose.Schema.Types.ObjectId, ref: 'Director' },
 Actors: [String],
 ImagePath: String,
 Featured: Boolean
