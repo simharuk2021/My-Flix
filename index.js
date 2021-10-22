ca59bd50bf725c7dd1d803bdcbe589b5364cdf47
simharuk2021 <83026016+simharuk2021@users.noreply.github.com>
2021-10-22 15:01:25 +0100

    POPULATE ADDED
    populated favourite movies

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ app.get('/users', passport.authenticate('jwt', {session: false}), (req, res) => 
 
 //Gets a user by username
 app.get('/users/:Username', passport.authenticate('jwt', {session: false}), (req, res) => {
-  Users.findOne({ Username: req.params.Username })
+  Users.findOne({ Username: req.params.Username }).populate('FavoriteMovies')
   .then((user) => {
     res.json(user);
   })
