cffa285d0b97acfddca30045982497a4824b186a
simharuk2021 <83026016+simharuk2021@users.noreply.github.com>
2021-11-08 14:00:07 +0000

    UPDATE USER PASWORD HASH ENABLED
    code introduced to ensure the password for put user is hashed due to a bug where an updated could no longer log in as a hashed password is required for the login endpoint

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ app.put('/users/:Username', passport.authenticate('jwt', {session: false}), (req
     $set:
     {
       Username: req.body.Username,
-      Password: req.body.Password,
+      Password: req.body.hashedPassword,
       Email: req.body.Email,
       Birthday: req.body.Birthday
     }
